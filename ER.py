c57814cc29da3d6056c409755f01760b6f12c2b6
RedboyZet135 <121138967+RedBOyC@users.noreply.github.com>
2025-04-20 21:22:12 +0800

    Add files via upload

---
+++ ER.py
@@ -0,0 +1,27 @@
+def ER(a, b, m):
+  if type(m)!=int:
+    return "m is not an integer!"
+  #ensure a and b are coprime
+  smallest=min(a, b)
+  divisor=[]
+  for d in range(a+1):
+    if a%d==0 and b%d==0:
+      divisor.append(d)
+  if len(divisor)>1:
+    return "a and b are not coprime!"
+  
+  A=[]
+  B=[]
+  primes=[]
+  for n in range(m+1):
+    if n not in B:
+      p=a*n + b 
+      A.append(n)
+      primes.append(p)
+      for k in range(m+1):
+        exempted=(p*k)+((p-b)//a)
+        if exempted<=m:
+          B.append(exempted)
+        else:
+          break
+  return A, B, primes
